ffb3d2bcf24d784df3ae97a209901b332d841a35
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-06-13 22:46:46 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+// import styles
 import './css/style.css'
-
+// import react router components
 import { Link, Routes, Route } from 'react-router-dom';
 
 // Components
@@ -7,14 +8,17 @@ import Home from './components/Home';
 import News from './components/News';
 import About from './components/About';
 
-export default function Appp() {
+export default function App() {
   return (
     <div>
+    // visibly navigation
       <div class='nav-bar'>
         <Link to="/Home"><button class='btn_nav'>Home</button></Link>
         <Link to="/News"><button class='btn_nav'>News</button></Link>
         <Link to="/About"><button class='btn_nav'>About</button></Link>
       </div>
+    
+    // Pages that will be visible when switching
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="Home" element={<Home />} />
