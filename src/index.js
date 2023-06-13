d1e41a28f735ddae147f51f74296c139348b2b28
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-06-13 22:48:31 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+// import deafault react and react-router components
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App'; 
 import { BrowserRouter } from 'react-router-dom';
-
+// render
 ReactDOM.render(
   <React.StrictMode>
+  // BrowserRouter shows in which js file the routing will take place
     <BrowserRouter>
       <App />
     </BrowserRouter>
