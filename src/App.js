377314b7774e00c856818562bec8edf2efaa944a
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-07-31 21:28:26 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import About from './components/About';
 export default function App() {
   return (
     <div>
-    // visibly navigation
+    // Visibly navigation
       <div class='nav-bar'>
         <Link to="/Home"><button class='btn_nav'>Home</button></Link>
         <Link to="/News"><button class='btn_nav'>News</button></Link>
