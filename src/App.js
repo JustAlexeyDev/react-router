531d0a640d27d7f0a7e63af3acbce0fc12dd8398
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2023-08-09 23:12:44 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,14 +11,12 @@ import About from './components/About';
 export default function App() {
   return (
     <div>
-    // Visibly navigation
       <div class='nav-bar'>
         <Link to="/Home"><button class='btn_nav'>Home</button></Link>
         <Link to="/News"><button class='btn_nav'>News</button></Link>
         <Link to="/About"><button class='btn_nav'>About</button></Link>
       </div>
-    
-    // Pages that will be visible when switching
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="Home" element={<Home />} />
